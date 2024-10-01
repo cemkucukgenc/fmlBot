f94edd0906cfed680faf3a9e4d19df75541172b6
ozerbar <ozerbar@hotmail.com>
2024-10-01 17:13:16 +0200

    before task3

--- Challenge/task_2.py
+++ Challenge/task_2.py
@@ -7,7 +7,7 @@ from FMLCamera import FMLCamera
 from FMLMqtt import FMLMqtt
 import time
 
-flag_qr_code_detected = False
+flag_qr_code_matched = False
 
 def check_qr_code_payload(camera, robot):
     while True:
@@ -19,10 +19,10 @@ def check_qr_code_payload(camera, robot):
     return qr_code_payload_check
 
 def compare_qr_code(qr_code_payload_check, target_payload_ID, robot):
-    global flag_qr_code_detected
+    global flag_qr_code_matched
     if qr_code_payload_check == target_payload_ID:
         print("QR Codes Matched")
-        flag_qr_code_detected = True
+        flag_qr_code_matched = True
         robot.drive(distance=0.10, velocity=300)
         robot.drop_fork()
         time.sleep(0.1)
@@ -30,14 +30,14 @@ def compare_qr_code(qr_code_payload_check, target_payload_ID, robot):
         time.sleep(0.1)
         robot.drive(distance=-0.10, velocity=300)
 
-def turn_right_at_blue(robot, camera, target_payload_ID, flag_qr_code_detected):
+def turn_right_at_blue(robot, camera, target_payload_ID, flag_qr_code_matched):
     ground_cam_left = robot.get_ground_cam_left()
 
     if ground_cam_left == "Blue" or "Green":
         print(ground_cam_left)
         print('detected color: {}'.format(ground_cam_left))
 
-        if not flag_qr_code_detected:
+        if not flag_qr_code_matched:
             robot.turn(90)
             qr_code_payload_check = check_qr_code_payload(camera, robot)
             compare_qr_code(qr_code_payload_check, target_payload_ID, robot)
@@ -49,7 +49,9 @@ def turn_right_at_blue(robot, camera, target_payload_ID, flag_qr_code_detected):
     
 
 def doTask(robot : FMLRobot,mqtt : FMLMqtt,camera: FMLCamera):
-    while True:
+    current_task_number = 2
+    global flag_qr_code_matched
+    while flag_qr_code_matched:
         target_payload_ID = camera.get_barcode()
         if robot.is_integer(target_payload_ID):  # Check if the returned string is a number
             target_payload_ID = int(target_payload_ID)  # Convert to an integer
@@ -60,42 +62,21 @@ def doTask(robot : FMLRobot,mqtt : FMLMqtt,camera: FMLCamera):
     robot.follower_line(velocity=300, controller=controller_line_following)
 
 
-    turn_right_at_blue(robot, camera, target_payload_ID, flag_qr_code_detected)
-    turn_right_at_blue(robot, camera, target_payload_ID, flag_qr_code_detected)
-    turn_right_at_blue(robot, camera, target_payload_ID, flag_qr_code_detected)
+    turn_right_at_blue(robot, camera, target_payload_ID, flag_qr_code_matched)
+    turn_right_at_blue(robot, camera, target_payload_ID, flag_qr_code_matched)
+    turn_right_at_blue(robot, camera, target_payload_ID, flag_qr_code_matched)
 
-    # print(ground_cam_left)
-    # if ground_cam_left == "Blue" or "Green":
-    #     print(ground_cam_left)
-        
-    #     print('detected color: {}'.format(ground_cam_left))
-
-    #     if not flag_qr_code_detected:
-    #         robot.turn(90)
-    #         qr_code_payload_check = check_qr_code_payload(camera, robot)
-    #         compare_qr_code(qr_code_payload_check, target_payload_ID, robot)
-    #         robot.turn(-90)
-
-    # robot.drive(distance=0.05, velocity=300)
-    # controller_line_following = PIController(kp=7.0,ki=0.02,target_value=30.0)
-    # robot.follower_line(velocity=300, controller=controller_line_following)
-
-    # if ground_cam_left == "Blue" or "Green":
-    #     print(ground_cam_left)
-    #     print('detected color: {}'.format(ground_cam_left))
-
-    #     if not flag_qr_code_detected:
-    #         robot.turn(90)
-    #         qr_code_payload_check = check_qr_code_payload(camera, robot)
-    #         compare_qr_code(qr_code_payload_check, target_payload_ID, robot)
-    #         robot.turn(-90)
-
-    # robot.drive(distance=0.05, velocity=300)
-    # controller_line_following = PIController(kp=7.0,ki=0.02,target_value=30.0)
-    # robot.follower_line(velocity=300, controller=controller_line_following)
-
+    ground_cam_left = robot.get_ground_cam_left()
+    if ground_cam_left == "Red":
+        print('detected color: {}'.format(ground_cam_left))
+        robot.turn(-90)
+        # print(camera.get_image_array())
+        while True:
+            current_task_number = camera.get_barcode()
+            if robot.is_integer(current_task_number):  # Check if the returned string is a number
+                current_task_number = int(current_task_number)  # Convert to an integer
+                break  # Break the loop when a valid integer is obtained
+        robot.turn(90)
     
+    return current_task_number
 
-    
-    
-    
--- Challenge/task_3.py
+++ Challenge/task_3.py
@@ -16,4 +16,6 @@ color_dict = {'a': "Blue", 'b': "Red", 'c': "Blue", 'd': "Blue", 'e': "Red",
                   'k': "Blue", 'l': "Blue", 'm': "Red", 'n': "Yellow", 'o': "Red"}
 
 def doTask(robot : FMLRobot,mqtt : FMLMqtt,camera: FMLCamera):
-    pass
+    robot.drive(distance=0.05, velocity=300)
+    controller_line_following = PIController(kp=7.0,ki=0.02,target_value=30.0)
+    robot.follower_line(velocity=300, controller=controller_line_following)
