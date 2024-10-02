33a9fbc61068a0bb3f38f325b24ec6f8b8e05993
ozerbar <ozerbar@hotmail.com>
2024-10-02 10:42:01 +0200

    dijkstra update

--- Challenge/dijkstra.py
+++ Challenge/dijkstra.py
@@ -1,50 +1,52 @@
 # Define the graph based on extracted connections
-graph = {"a": {"b": 2, "d": 5, "f": 6},
-          "b": {"c": 3, "e": 2},
-          "c": {"g": 8, "e": 1},
-          "d": {"e": 1, "h": 10},
-          "e": {"g": 10, "h": 10},
-          "f": {"d": 2, "i": 4},
-          "g": {"j": 8},
-          "h": {"j": 6, "k": 6},
-          "i": {"h": 7, "l": 4},
-          "j": {"n": 5, "k": 7},
-          "k": {"i": 7, "n": 9},
-          "l": {"m": 9},
-          "m": {"k": 1},
-          "n": {"o": 1},
-          "o": {},
+graph = {
+    "a": {"b": 2, "d": 5, "f": 6},
+    "b": {"c": 3, "e": 2},
+    "c": {"g": 8, "e": 1},
+    "d": {"e": 1, "h": 10},
+    "e": {"g": 10, "h": 10},
+    "f": {"d": 2, "i": 4},
+    "g": {"j": 8},
+    "h": {"j": 6, "k": 6},
+    "i": {"h": 7, "l": 4},
+    "j": {"n": 5, "k": 7},
+    "k": {"i": 7, "n": 9},
+    "l": {"m": 9},
+    "m": {"k": 1},
+    "n": {"o": 1},
+    "o": {},
 }
 
+# Define the colors for the paths
 path_colors = {
-    ("a", "b"): "red",
-    ("a", "d"): "blue",
-    ("a", "f"): "yellow",
-    ("b", "c"): "blue",
-    ("b", "e"): "red",
-    ("c", "g"): "blue",
-    ("c", "e"): "red",
-    ("d", "e"): "red",
-    ("d", "h"): "yellow",
-    ("e", "g"): "blue",
-    ("e", "h"): "yellow",
-    ("f", "d"): "blue",
-    ("f", "i"): "red",
-    ("g", "j"): "red",
-    ("h", "j"): "red",
-    ("h", "k"): "blue",
-    ("i", "h"): "yellow",
-    ("i", "l"): "blue",
-    ("j", "n"): "yellow",
-    ("j", "k"): "blue",
-    ("k", "i"): "red",
-    ("k", "n"): "yellow",
-    ("l", "m"): "red",
-    ("m", "k"): "blue",
-    ("n", "o"): "red",
+    ("a", "b"): "Red",
+    ("a", "d"): "Blue",
+    ("a", "f"): "Yellow",
+    ("b", "c"): "Blue",
+    ("b", "e"): "Red",
+    ("c", "g"): "Blue",
+    ("c", "e"): "Red",
+    ("d", "e"): "Red",
+    ("d", "h"): "Yellow",
+    ("e", "g"): "Blue",
+    ("e", "h"): "Yellow",
+    ("f", "d"): "Blue",
+    ("f", "i"): "Red",
+    ("g", "j"): "Red",
+    ("h", "j"): "Red",
+    ("h", "k"): "Blue",
+    ("i", "h"): "Yellow",
+    ("i", "l"): "Blue",
+    ("j", "n"): "Yellow",
+    ("j", "k"): "Blue",
+    ("k", "i"): "Red",
+    ("k", "n"): "Yellow",
+    ("l", "m"): "Red",
+    ("m", "k"): "Blue",
+    ("n", "o"): "Red",
 }
 
-# Implement Dijkstra's algorithm using the existing function
+# Function to perform Dijkstra's algorithm
 def dijkstra(graph, startnode, endnode):
     D = {node: float('inf') for node in graph}  # Distance dictionary
     D[startnode] = 0
@@ -54,8 +56,6 @@ def dijkstra(graph, startnode, endnode):
     while MK:
         # Find the node with the smallest distance in MK
         current_node = min(MK, key=lambda node: D[node])
-
-        # Remove it from MK
         MK.remove(current_node)
 
         # Check neighbors
@@ -70,24 +70,25 @@ def dijkstra(graph, startnode, endnode):
     # Return the shortest path from startnode to endnode
     return R[endnode] + [endnode]
 
-
 # Function to get the color list for a given path
 def get_path_colors(path, path_colors):
     color_list = []
-    # Iterate through the path to get the colors for each segment
     for i in range(len(path) - 1):
-        # Get the color for the segment (path[i], path[i+1])
+        # Get the color for the segment (path[i], path[i+1]) or (path[i+1], path[i])
         segment_color = path_colors.get((path[i], path[i + 1])) or path_colors.get((path[i + 1], path[i]))
         if segment_color:
             color_list.append(segment_color)
     return color_list
 
-# # Example usage with the Dijkstra path
-# start = "a"
-# end = "n"
-# path = dijkstra(graph, start, end)
-# print("Shortest path from {} to {}:".format(start, end), path)
+# Modular function to get the path as a color list
+def get_path_color_list(start, end, graph, path_colors):
+    # Get the shortest path using Dijkstra's algorithm
+    path = dijkstra(graph, start, end)
+    # Get the color list for the path
+    return get_path_colors(path, path_colors)
 
-# # Get the color list for the path
-# color_list = get_path_colors(path, path_colors)
-# print("Color list for the path {}: {}".format(path, color_list))
+# # Example usage of the modular function
+# start_point = "a"
+# end_point = "n"
+# color_list = get_path_color_list(start_point, end_point, graph, path_colors)
+# print(color_list)
--- Challenge/task_3.py
+++ Challenge/task_3.py
@@ -6,8 +6,9 @@ from FMLRobot import FMLRobot
 from FMLController import PIController
 from FMLCamera import FMLCamera
 from FMLMqtt import FMLMqtt
-import dijkstra
+from dijkstra import *
 import time
+
 
 
 
@@ -15,43 +16,110 @@ color_dict = {'a': "Blue", 'b': "Red", 'c': "Blue", 'd': "Blue", 'e': "Red",
                   'f': "Yellow", 'g': "Blue", 'h': "Yellow", 'i': "Red", 'j': "Red",
                   'k': "Blue", 'l': "Blue", 'm': "Red", 'n': "Yellow", 'o': "Red"}
 
-def doTask(robot : FMLRobot,mqtt : FMLMqtt,camera: FMLCamera):
+
+
+
+
+def go_to_color(robot, color_list, ground_cam_left):
+    for i in color_list:
+        target_color = i
+        print ("Next point is : {}".format(i))
+
+        print ("Color {} searching".format(i))
+        robot.turn(15)
+        time.sleep(0.1)
+        ground_cam_left = robot.get_ground_cam_left()
+        print(f'detected color: {ground_cam_left}')
+
+        if ground_cam_left == target_color:
+            print("Target color: {} found".format(ground_cam_left))
+            robot.turn(-10)
+            robot.drive(distance=0.10, velocity=300)
+            time.sleep(0.5)
+            controller_line_following = PIController(kp=7.0, ki=0.02, target_value=30.0)
+            robot.follower_line(velocity=300, controller=controller_line_following)
+
+            # Update ground_cam_left after moving
+            ground_cam_left = robot.get_ground_cam_left()
+            if ground_cam_left == "Red":
+                print("Red stop sign detected but passing")
+                robot.drive(distance=0.21, velocity=300)
+    return ground_cam_left
+
+def doTask(robot: FMLRobot, mqtt: FMLMqtt, camera: FMLCamera):
     robot.drive(distance=0.05, velocity=300)
-    controller_line_following = PIController(kp=7.0,ki=0.02,target_value=30.0)
+    controller_line_following = PIController(kp=7.0, ki=0.02, target_value=30.0)
     robot.follower_line(velocity=300, controller=controller_line_following)
     
     time.sleep(0.1)
     ground_cam_left = robot.get_ground_cam_left()
-    print('detected color: {}'.format(ground_cam_left))
-
-    # while True:
-    #     ground_cam_left_from_func = robot.get_ground_cam_left()
-    #     print('detected color from func: {}'.format(ground_cam_left_from_func))
-
-
+    print(f'detected color: {ground_cam_left}')
 
     if ground_cam_left == "Red":
         print("Red detected but passing")
         robot.drive(distance=0.21, velocity=300)
 
     while True:
-        robot.turn(15)
-        time.sleep(0.1)
-        ground_cam_left = robot.get_ground_cam_left()
-        print('detected color: {}'.format(ground_cam_left)) 
+
+        start_point = "a"
+        end_point = "n"
+        color_list = get_path_color_list(start_point, end_point, graph, path_colors)
+        print(color_list)
+        
+        # Pass the ground_cam_left value to the go_to_color function
+        ground_cam_left = go_to_color(robot, color_list, ground_cam_left)
 
 
-        target_color = "Blue"  # Define the target color here
-        if ground_cam_left == target_color:
-            robot.turn(-10)
-            robot.drive(distance=0.10, velocity=300)
-            time.sleep(0.5)
-            controller_line_following = PIController(kp=7.0,ki=0.02,target_value=30.0)
-            robot.follower_line(velocity=300, controller=controller_line_following)
+# def go_to_color(robot, color_list):
+#     for i in color_list:
+#         target_color = i
 
-            ground_cam_left = robot.get_ground_cam_left()
-            if ground_cam_left == "Red":
-                print("Red stop sign detected but passing")
-                robot.drive(distance=0.21, velocity=300)
+#         if ground_cam_left == target_color:
+#             robot.turn(-10)
+#             robot.drive(distance=0.10, velocity=300)
+#             time.sleep(0.5)
+#             controller_line_following = PIController(kp=7.0,ki=0.02,target_value=30.0)
+#             robot.follower_line(velocity=300, controller=controller_line_following)
+
+#             ground_cam_left = robot.get_ground_cam_left()
+#             if ground_cam_left == "Red":
+#                 print("Red stop sign detected but passing")
+#                 robot.drive(distance=0.21, velocity=300)
+
+# def doTask(robot : FMLRobot,mqtt : FMLMqtt,camera: FMLCamera):
+#     robot.drive(distance=0.05, velocity=300)
+#     controller_line_following = PIController(kp=7.0,ki=0.02,target_value=30.0)
+#     robot.follower_line(velocity=300, controller=controller_line_following)
+    
+#     time.sleep(0.1)
+#     ground_cam_left = robot.get_ground_cam_left()
+#     print('detected color: {}'.format(ground_cam_left))
+
+#     # while True:
+#     #     ground_cam_left_from_func = robot.get_ground_cam_left()
+#     #     print('detected color from func: {}'.format(ground_cam_left_from_func))
+
+
+
+#     if ground_cam_left == "Red":
+#         print("Red detected but passing")
+#         robot.drive(distance=0.21, velocity=300)
+
+#     while True:
+#         robot.turn(15)
+#         time.sleep(0.1)
+#         ground_cam_left = robot.get_ground_cam_left()
+#         print('detected color: {}'.format(ground_cam_left))
+
+#         start_point = "a"
+#         end_point = "n"
+#         color_list = get_path_color_list(start_point, end_point, graph, path_colors)
+#         print(color_list)
+#         go_to_color(robot, color_list) 
+
+
+
+
+
 
 
